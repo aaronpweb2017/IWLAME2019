ebbc408cf8cdf488d576f1a54658eaecd2cb6f05
aaronpweb2017 <luis-aaron40@hotmail.com>
2019-02-11 22:07:22 -0700

    WebApplication1Commit3

---
+++ WebApplication1/WebApplication1/Pages/Create.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext DataBase;
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public CreateModel(AppDbContext DataBase) { this.DataBase = DataBase; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+            DataBase.Customers.Add(Customer);
+            await DataBase.SaveChangesAsync();
+            Message = $"Customer '{Customer.Name}' added!!!";
+            return RedirectToPage("/Index");
+        }
+    }
+}
--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        [TempData]
+        public string Message { get; set; }
 
+        private readonly AppDbContext DataBase;
+
+        public IndexModel(AppDbContext DataBase)
+        {
+            this.DataBase = DataBase;
+        }
+
+        public IList<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync() //async --> to avoid bottlenecks:
+        {
+            Customers = await DataBase.Customers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var Customer = await DataBase.Customers.FindAsync(id);
+            if (Customer != null)
+            {
+                DataBase.Customers.Remove(Customer);
+                await DataBase.SaveChangesAsync();
+                Message = $"Customer '{Customer.Name}' deleted!!!";
+            }
+            return RedirectToPage();
         }
     }
 }
