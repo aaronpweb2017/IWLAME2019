a78d8522571c5bc492eb822f5b51c49a4a0fa13c
aaronpweb2017 <luis-aaron40@hotmail.com>
2019-02-14 16:31:11 -0700

    RepasoProgWebCommit2

--- RepasoProgWeb/Registration/Register.php
+++ RepasoProgWeb/Registration/Register.php
@@ -1,3 +1,4 @@
+<?php include('server.php'); ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,13 +10,15 @@
 			<h2>Register</h2>
 		</div>
 		<form method="post" action="register.php">
+			<!-- display validation errors here: -->
+			<?php include('errors.php'); ?>	
 			<div class="input-group">
 				<label>Username</label>
-				<input type="text" name="username">
+				<input type="text" name="username" value="<?php echo $username ?>">
 			</div>
 			<div class="input-group">
 				<label>Email</label>
-				<input type="text" name="email">
+				<input type="text" name="email" value="<?php echo $email ?>">
 			</div>
 			<div class="input-group">
 				<label>Password</label>
@@ -28,6 +31,9 @@
 			<div class="input-group">
 				<button type="submit" name="register" class="btn">Register</button>
 			</div>
+			<p>
+				Already a member? <a href="login.php">Sign in</a>
+			</p>
 		</form>
 	</body>
 </html>
---
+++ RepasoProgWeb/Registration/server.php
@@ -0,0 +1,84 @@
+<?php
+	session_start();
+
+	$username = "";
+	$email = "";
+	$errors = array();
+
+	//Connect to the database:
+	$db = mysqli_connect('localhost', 'root', '', 'registration');
+
+	//If the register button is clicked:
+	if (isset($_POST['register'])) {
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
+		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+		//ensure that form fields are filled properly:
+		if(empty($username)) {
+			array_push($errors, "Username is required.");
+		}
+		if(empty($email)) {
+			array_push($errors, "Email is required.");
+		}
+		if(empty($password_1)) {
+			array_push($errors, "Password is required.");
+		} 
+		if($password_1!=$password_2) {
+			array_push($errors, "The two passwords do not match.");
+		}
+
+		//if there are no errors, save user to database:
+		if(count($errors)==0) { 
+			$key = random_int(1, 1000);
+			$password = md5($password_1); //encrypt password before storing it.
+			$sql = "INSERT INTO users (id, username, email, password) VALUES ($key, $username, $email, $password)";
+			mysqli_query($db, $sql);
+			echo "You have been registered.";
+			$_SESSION['username'] = $username;
+			$_SESSION['success'] = "you're now logged in.";
+			header('location: index.php'); //redirect to home page;
+		}
+	}
+
+	//log user in from login page:
+	if (isset($_POST['login'])) {
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$password = mysqli_real_escape_string($db, $_POST['password']);
+
+		//ensure that form fields are filled properly:
+		if(empty($username)) {
+			array_push($errors, "Username is required.");
+		}
+		if(empty($password)) {
+			array_push($errors, "Password is required.");
+		}
+
+
+		//if there are no errors, find user in database:
+		if(count($errors)==0) { 
+			$password = md5($password); //encrypt password before comparing.
+			$query = "SELECT* FROM users WHERE username='$username' AND password='$password'";
+			$result = mysqli_query($db, $query);
+			if(mysqli_num_rows($result) == 1) {
+				//log user in:
+				$_SESSION['username'] = $username;
+				$_SESSION['success'] = "you're now logged in.";
+				header('location: index.php'); //redirect to home page;
+			} 
+			else {
+				array_push($errors, "wrong username/password combination.");
+			}
+		}
+
+
+	}
+
+	//logout
+	if(isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header('location: login.php');
+	}
+?>
