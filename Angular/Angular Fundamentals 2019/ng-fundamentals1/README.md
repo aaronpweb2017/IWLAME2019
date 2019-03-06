c940216abf3353570bc583d0ce92fa10c247b6ee
aaronpweb2017 <luis-aaron40@hotmail.com>
2019-03-05 18:22:08 -0700

    AngularFundamentals2019Commit2

--- Angular/Angular Fundamentals 2019/ng-fundamentals1/README.md
+++ Angular/Angular Fundamentals 2019/ng-fundamentals1/README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+Problem File:
+C:\Users\e-lmedina\Documents\RepositoriosGitHub\IWLAME2019\Angular\Angular Fundamentals 2019\ng-fundamentals1\node_modules\webdriver-js-extender\built\built\built\built\built\built\built\built\built\built\built\built\built\built\built\built\spec\command_tests
--- Angular/Angular Fundamentals 2019/ng-fundamentals1/src/app/events/event-thumbnail.component.ts
+++ Angular/Angular Fundamentals 2019/ng-fundamentals1/src/app/events/event-thumbnail.component.ts
@@ -2,19 +2,33 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: './event-thumbnail.component.html'
+    templateUrl: './event-thumbnail.component.html',
+    styles: [`
+        .red { color: #FF0000 !important; }
+        .green { color: #00FF00 !important; }
+        .blue { color: #0000FF !important; }
+        .yellow { color: #FFFF00 !important; }
+        .bold { font-weight: bold; }
+        .oblique { font-style: oblique; }
+        .italic { font-style: italic; }
+        .normal { font-style: normal; }
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin-left: 10px; }
+        .well_div { color: #bbb; }
+        `
+    ]
 })
 
 export class EventThumbnailComponent {
-    @Input() employee : any
-    @Input() employeesQuantity : any 
+    @Input() event : any
+    @Input() eventsQuantity : any 
     @Output() eventClick = new EventEmitter()
     handleClickMe() {
-        alert('Some employee clicked...');
-        console.log('Some employee clicked...');
-        this.eventClick.emit(this.employee.id);
+        alert('Some event clicked...');
+        console.log('Some event clicked...');
+        this.eventClick.emit(this.event.id);
     }
-    showEmployeesQuantity() {
-        alert('CurrentEmployeesQuantity: '+this.employeesQuantity);
+    showSessionsQuantity() {
+        alert('CurrentSessionsQuantity: '+this.event.sessions.length);
     }
 }
